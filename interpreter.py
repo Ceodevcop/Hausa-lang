5aa5d8f8e716e157ec1a4d78538fb0b34b6fe144
Ahmad Hamza <priahmz@gmail.com>
2025-01-31 23:21:55 +0100

    Create interpreter.py

---
+++ interpreter.py
@@ -0,0 +1,26 @@
+def interpret(ast, variables=None):
+    if variables is None:
+        variables = {}
+
+    if ast[0] == 'program':
+        for statement in ast[1]:
+            interpret(statement, variables)
+    elif ast[0] == 'print':
+        print(ast[1].strip('"'))
+    elif ast[0] == 'assign':
+        variables[ast[1]] = ast[2]
+    elif ast[0] == 'loop':
+        for _ in range(variables[ast[1]]):
+            interpret(ast[2], variables)
+
+# Example usage
+ast = parser.parse("""
+fara
+    rubuta "Barka da zuwa!"
+    lamba nisa daidai 3
+    sau nisa
+        rubuta "Ana ci gaba da tafiya..."
+    karshe
+karshe
+""")
+interpret(ast)
